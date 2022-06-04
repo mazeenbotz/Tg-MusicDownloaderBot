402d71e974bda8663e164f45f6fa6a9ddb847136
Mazeen Ahamed <104501602+mazeenbotz@users.noreply.github.com>
2022-06-04 12:17:52 +0530

    Update botuse.py

--- botuse.py
+++ botuse.py
@@ -1,3 +1,13 @@
+class Chat:
+    def __init__(self, msg):
+        self.chat_id = msg['chat']['id']
+        self.user_input = msg['text']
+        self.user_input = self.user_input.replace('@MZN_MusicDownloaderBot', '')
+        self.user_name = msg['from']['first_name']
+        self.message_id = msg['message_id']
+
+        self.messages = {
+            'king': 'Our Bots'
 START_BUTTONS = InlineKeyboardMarkup(
     [
         [
