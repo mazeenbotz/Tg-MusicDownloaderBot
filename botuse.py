47e75ad35e3ec72ac7d397c6c6042a744aa16bce
Mazeen Ahamed <104501602+mazeenbotz@users.noreply.github.com>
2022-06-04 12:16:48 +0530

    Create botuse.py

---
+++ botuse.py
@@ -0,0 +1,8 @@
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("📨 Support", url="https://t.me/JaguarBots"),
+            InlineKeyboardButton("📚 Source Code", url="https://github.com/ImJanindu/47MusicPlayerBot")
+        ]
+    ]
+)
